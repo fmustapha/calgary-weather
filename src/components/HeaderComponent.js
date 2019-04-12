2250c7b7c6ad593c1cf5eec8fc01f5abcc8c5d43
Faith Mustapha <f81@MacBook-Air.local>
2019-04-12 16:43:13 -0600

    add app subtitle

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -4,6 +4,7 @@ const HeaderComponent = () => {
         return ( 
             <header>
                 <h1>Calgary Weather Forcast</h1>
+                <p>View 3 hour forcast for three days below</p>
             </header>
          );
 }
