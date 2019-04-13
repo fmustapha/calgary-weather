863d35ea143d5dac8d0e2c0541ef5395f0012632
Faith Mustapha <f81@MacBook-Air.local>
2019-04-13 06:08:53 -0600

    modify header component, add unit tests and install dependencies

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-const HeaderComponent = () => {
+const HeaderComponent = (props) => {
         return ( 
             <header>
+                <div className="heading">
                 <h1>Calgary Weather Forcast</h1>
-                <p>View 3 hour forcast for three days below</p>
+                </div>
+                <p className="description">View 3 hour forcast for three days below</p>
             </header>
          );
 }
